e35a641ce8f42020e013ab6b1b6a3b821d33ccb6
redh3ad <play.filin@gmail.com>
2021-04-08 00:26:40 +0300

    feat: add the right decision

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,24 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  for (let i = 0; i < 1; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      sum += matrix[i][j];
+    }
+  }
+
+  return sum;
 }
+
+/* throw new Error('Not implemented'); */
 
 module.exports = getMatrixElementsSum;
