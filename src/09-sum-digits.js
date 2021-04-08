53df0ea1b36e294914c97a3e63ebf6d394c73aee
redh3ad <play.filin@gmail.com>
2021-04-09 01:24:16 +0300

    feat: add the right decision

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,18 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let num = n;
+  while (num > 9) {
+    let sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num -= num % 10;
+      num /= 10;
+    }
+    num = sum;
+  }
+  return num;
 }
 
 module.exports = getSumOfDigits;
