c251fd13db8b7379b79c3511273b8dc64cf002d3
redh3ad <play.filin@gmail.com>
2021-04-09 01:49:47 +0300

    feat: add the right decision

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const a = [];
+  for (let i = 0; i < str.length; i++) {
+    let count = 1;
+    while (i < str.length - 1 && str[i] === str[i + 1]) {
+      count++;
+      i++;
+    }
+    if (count === 1) {
+      a.push(`${str[i]}`);
+    } else {
+      a.push(`${count}${str[i]}`);
+    }
+  }
+  return a.join('');
 }
 
 module.exports = encodeLine;
