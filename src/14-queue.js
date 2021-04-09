111ba6e9f7ed8d71cbe54612112d17f15ba4c20a
redh3ad <play.filin@gmail.com>
2021-04-09 13:09:40 +0300

    feat: add the right decision

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +12,23 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.queue = [];
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.queue.length;
+  }
+
+  enqueue(element) {
+    const node = new ListNode(element);
+    node.next = null;
+    this.queue.push(element);
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const node = this.queue.shift();
+    return node;
   }
 }
 
